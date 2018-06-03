2fddedf9642487af0e487dc1580bca115159a3b7
Grégoire Weber <gweber@clever-age.com>
2018-06-03 16:24:43 -0400

    Add env variables to setup database from docker-compose

--- drupal/docker-entrypoint.sh
+++ drupal/docker-entrypoint.sh
@@ -1,13 +1,55 @@
 #!/bin/sh -ex
 
-while ! mysqladmin ping -h "drupal-db" -u drupal -pdrupal --silent; do
-    sleep 1
-done
+if [[ "$1" == start ]]; then
 
-drush si standard \
-  --db-url=mysql://drupal:drupal@drupal-db/drupal \
-  -n -y \
-  --account-name drupal \
-  --account-pass drupal368
+  if [ ! -f .installed ]; then
+    echo "Drupal is not installed yet."
 
-docker-php-entrypoint php-fpm
+    if [ -z "${MYSQL_HOST}" ]; then
+      echo "Environment variable MYSQL_HOST is needed."
+      exit 1
+    fi
+
+    if [ -z "${MYSQL_DATABASE}" ]; then
+      echo "Environment variable MYSQL_DATABASE is needed."
+      exit 1
+    fi
+
+    if [ -z "${MYSQL_USER}" ]; then
+      echo "Environment variable MYSQL_USER is needed."
+      exit 1
+    fi
+
+    if [ -z "${MYSQL_PASSWORD}" ]; then
+      echo "Environment variable MYSQL_PASSWORD is needed."
+      exit 1
+    fi
+
+    if [ -z "${ADMIN_USERNAME}" ]; then
+      echo "Environment variable ADMIN_USERNAME is needed."
+      exit 1
+    fi
+
+    if [ -z "${ADMIN_PASSWORD}" ]; then
+      echo "Environment variable ADMIN_PASSWORD is needed."
+      exit 1
+    fi
+
+    while ! mysqladmin ping -h "$MYSQL_HOST" -u $MYSQL_USER -p$MYSQL_PASSWORD --silent; do
+        sleep 1
+    done
+
+    drush si standard \
+      --db-url=mysql://$MYSQL_USER:$MYSQL_PASSWORD@$MYSQL_HOST/$MYSQL_DATABASE \
+      -n -y \
+      --account-name $ADMIN_USERNAME \
+      --account-pass $ADMIN_PASSWORD
+
+    touch .installed
+  fi
+
+  docker-php-entrypoint php-fpm
+
+else
+  echo "Specify command to run on startup"
+fi
